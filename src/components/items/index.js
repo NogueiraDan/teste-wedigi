3eaf44bdad8ab293b023a8961ab530aa69d98f55
Daniel Nogueira <silvanogueira3460@gmail.com>
2022-07-09 18:42:28 -0300

    Subitens editaveis

--- src/components/items/index.js
+++ src/components/items/index.js
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
 import * as S from "./styled";
 import Swal from "sweetalert2";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Item = ({ indexList }) => {
   const [text, setText] = useState();
+  const [textModal, setTextModal] = useState("");
   const [items, setItems] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   let idItem = indexList;
 
+  // Capturando estado do input do Modal
+  const handleChangeTitle = (event) => {
+    const inputTitle = event.target.value;
+    setTextModal(inputTitle);
+  };
+
+  // Alterando titulo da sublista
+  const handleUpdateItem = (key) => {
+    const itensCopy = Array.from(items);
+    itensCopy.splice(key, 1, textModal);
+    setItems(itensCopy);
+    localStorage.setItem("item", JSON.stringify(itensCopy));
+    setTextModal("");
+    handleClose();
+  };
+
+  // Capturando estado do input da sublista
   const handleChangeInput = (event) => {
     const inputItem = event.target.value;
     setText(inputItem);
   };
 
+  // Adicionando sublista à lista
   const handleAddItem = (event) => {
     event.preventDefault();
     if (!text) {
@@ -38,10 +63,11 @@ const Item = ({ indexList }) => {
           ])
         );
       }
-      console.log(`Id do ITEM ${modelStorage.nome} é: ${idItem}`);
       setText("");
     }
   };
+
+  // Removendo sublista da lista
   const handleDeleteList = (key) => {
     const removeList = Array.from(items);
     removeList.splice(key, 1);
@@ -68,6 +94,32 @@ const Item = ({ indexList }) => {
           <S.RemoveIcon onClick={() => handleDeleteList(key)}>
             <i class="ai-trash-can"></i>
           </S.RemoveIcon>
+          <span onClick={handleShow}>
+            <i class="ai-pencil"></i>
+          </span>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Body>
+              <Form>
+                <Form.Group>
+                  <Form.Label>Edite o titulo</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Novo titulo"
+                    autoFocus
+                    onChange={(event) => handleChangeTitle(event)}
+                  />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button variant="primary" onClick={() => handleUpdateItem(key)}>
+                Salvar
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </S.MapItems>
       ))}
     </>
